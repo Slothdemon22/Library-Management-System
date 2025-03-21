7006d1141c64e29ab14ac4a007293b4c1966ddd0
Slothdemon <www.basilslothdemon@gmail.com>
2025-03-21 17:23:21 +0500

    user and admin classes

--- backend/dist/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { createUser, login, logout } from "../controllers/auth.js";
-const router = express.Router();
-router.post('/register', createUser);
-router.post('/login', login);
-router.post('/logout', logout);
-export default router;
--- backend/src/routes/authRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var auth_1 = require("../controllers/auth");
-var router = express_1.default.Router();
-router.post('/register', auth_1.createUser);
-exports.default = router;
